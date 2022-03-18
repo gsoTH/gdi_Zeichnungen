c15b73cf22f6f84fa005bbbb6ad47efb8443bfbf
M.Thomas <th@gso-koeln.de>
2022-03-18 12:40:51 +0100

    Vorlage für eine Aufgabe hinzugefügt.

---
+++ Zeichnungen/Vorlage/FrmDummy.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Zeichnungen
+{
+    partial class FrmDummy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDummy));
+            this.cbxShowGoal = new System.Windows.Forms.CheckBox();
+            this.pbxGoal = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGoal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxShowGoal
+            // 
+            this.cbxShowGoal.AutoSize = true;
+            this.cbxShowGoal.Checked = true;
+            this.cbxShowGoal.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbxShowGoal.Location = new System.Drawing.Point(22, 17);
+            this.cbxShowGoal.Name = "cbxShowGoal";
+            this.cbxShowGoal.Size = new System.Drawing.Size(196, 36);
+            this.cbxShowGoal.TabIndex = 0;
+            this.cbxShowGoal.Text = "Ziel anzeigen ";
+            this.cbxShowGoal.UseVisualStyleBackColor = true;
+            this.cbxShowGoal.CheckedChanged += new System.EventHandler(this.cbxShowGoal_CheckedChanged);
+            // 
+            // pbxGoal
+            // 
+            this.pbxGoal.Image = ((System.Drawing.Image)(resources.GetObject("pbxGoal.Image")));
+            this.pbxGoal.Location = new System.Drawing.Point(18, 77);
+            this.pbxGoal.Name = "pbxGoal";
+            this.pbxGoal.Size = new System.Drawing.Size(1041, 790);
+            this.pbxGoal.TabIndex = 1;
+            this.pbxGoal.TabStop = false;
+            // 
+            // FrmDummy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1086, 894);
+            this.Controls.Add(this.pbxGoal);
+            this.Controls.Add(this.cbxShowGoal);
+            this.Name = "FrmDummy";
+            this.Text = "Schachbrett";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGoal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbxShowGoal;
+        private System.Windows.Forms.PictureBox pbxGoal;
+    }
+}
