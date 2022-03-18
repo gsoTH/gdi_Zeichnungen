1592bf070d0dcef0e7da8d35c8cf276790121fb4
M.Thomas <th@gso-koeln.de>
2022-03-18 12:19:08 +0100

    Ausgangslage hergestellt.

---
+++ Zeichnungen/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Zeichnungen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(586, 96);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Für jede Aufgabe gibt es eine Form.\r\n\r\nIm Branch \"loesungen\" finden Sie Lösungsvo" +
+    "rschläge.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(546, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 51);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Viel Spaß!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 609);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
